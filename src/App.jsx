2ec21e7b3c59aa48be58f81ba87ab60cd24464d3
vishwa-akshat <akvs0707@gmail.com>
2023-05-04 22:36:51 +0530

    feat: folder structure added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import Home from "pages/Home";
 import "./App.scss";
 
 function App() {
-    return <>Weather App</>;
+    return (
+        <div className="App">
+            <Home />
+        </div>
+    );
 }
 
 export default App;
